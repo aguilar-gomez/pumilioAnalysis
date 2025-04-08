1dc3a23730f58ab5b958437afef266786803061e
Andrew H Vaughn <ahv147@gmail.com>
2025-04-07 18:28:25 -0600

    Add files via upload

---
+++ 7_Demography/AdmixtureBayesREsults/2events/RunAnalysis.sh
@@ -0,0 +1,4 @@
+ 
+python3.10 ../../AdmixtureBayes-main/admixturebayes/analyzeSamples.py  --mcmc_results  Frog2/output1.csv  --burn_in_fraction 0.3
+
+python3.10 ../../AdmixtureBayes-main/admixturebayes/makePlots.py --plot top_trees --posterior thinned_samples.csv  --write_rankings chain3subs.txt
---
+++ 7_Demography/AdmixtureBayesREsults/3events/RunAnalysis.sh
@@ -0,0 +1,4 @@
+ 
+python3.10 ../../AdmixtureBayes-main/admixturebayes/analyzeSamples.py  --mcmc_results  Frog2/output1.csv  --burn_in_fraction 0.5
+
+python3.10 ../../AdmixtureBayes-main/admixturebayes/makePlots.py --plot top_minimal_topologies  --posterior thinned_samples.csv  --write_rankings chain3.txt
